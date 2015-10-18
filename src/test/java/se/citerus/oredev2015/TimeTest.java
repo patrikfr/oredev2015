cad39fb6de377fc9ee42ca62b14926b362820a66
Patrik Fredriksson <patrik@snart.nu>
2015-10-18 09:52:09 +0200

    Cleanup

--- src/test/java/se/citerus/oredev2015/TimeTest.java
+++ src/test/java/se/citerus/oredev2015/TimeTest.java
@@ -3,20 +3,15 @@ package se.citerus.oredev2015;
 
 import org.junit.Test;
 
-import java.math.BigDecimal;
 import java.time.Duration;
-import java.time.temporal.TemporalUnit;
 
 public class TimeTest {
 
     @Test
     public void marathonPace() {
-
         int distance = 42195;
-        Duration duration = Duration.ofHours(4);
-        Duration pace = duration.dividedBy(distance).multipliedBy(1000);
+        Duration goal = Duration.ofHours(4);
+        Duration pace = goal.dividedBy(distance).multipliedBy(1000);
         System.out.println(pace);
     }
-
-
 }
