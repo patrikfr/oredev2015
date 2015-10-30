0427dbd3d4de1f40d811f993d7c7f848396548b9
Patrik Fredriksson <patrik@snart.nu>
2015-10-30 20:38:56 +0100

    JDBC

---
+++ src/test/java/se/citerus/oredev2015/jdbc/PostgreSqlTest.java
@@ -0,0 +1,15 @@
+package se.citerus.oredev2015.jdbc;
+
+import java.time.LocalDateTime;
+
+public class PostgreSqlTest {
+
+    public void writeLocalDateTime() {
+
+        LocalDateTime time = LocalDateTime.of(2015, 11, 5, 12, 10);
+
+
+
+    }
+
+}
